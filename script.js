58df860aa5d9ba9da346af564d54c5ee4feeb390
Sergey <sergei.merlow@ya.ru>
2020-05-12 20:08:22 +0300

    first sborca

---
+++ script.js
@@ -0,0 +1,18 @@
+const numberOfFilms = +prompt('сколько фильмов вы уже просмотрели?', '')
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+const a = prompt('один из последних просмотренных фильмов?', ''),
+      b = prompt('на сколько оцените его?', ''),
+      c = prompt('один из последних просмотренных фильмов?', ''),
+      d = prompt('на сколько оцените его?', '');
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+console.log(personalMovieDB);    
